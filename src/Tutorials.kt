fec8ce62b6dc68ae41e5c8c0f6540d25c86248ee
RowBowl <robelrb@gmail.com>
2023-08-25 09:54:26 +0200

    for loops

--- src/Tutorials.kt
+++ src/Tutorials.kt
@@ -1,57 +1,21 @@
 
 
 fun main () {
-//https://www.youtube.com/playlist?list=PLQkwcJG4YTCRSQikwhtoApYs9ij_Hc5Z9
-    /*    ********Videos part 1-7
-    //println("hello world")
+    var arr =  arrayOf(1,2,3)
 
-    //"Int" optional
-    //var myVariable: Int = 5
-    var myVariable = 5
-    println(myVariable)
-    //$ before word in a string will interpolate the value of a variable by that name
-    myVariable += 1
-    println("The value of my variable is: $myVariable")
-
-    val myConstantVariable = 5
-    println("My constant variable is: $myConstantVariable")
-
-    var myBooleanVariable = true
-    println("My boolean variable is: $myBooleanVariable")
-
-    var x = 5
-    var y = 7
-    println("The sum of $x and $y is ${x+y}")
-
-    var xDecimal: Float = 5F
-    var yDecimal = 10F
-    println(xDecimal)
-    println(xDecimal/yDecimal)
-
-    var myStringVariable = "stRing variabEe TEST"
-    println(myStringVariable.uppercase().lowercase())
-    */
-
-    //
-//    ********Videos part 8-??
-/*
-    val x = 5
-    val y = 6
-    val a = 6
-    val b = 10
-    //AND will take precedance over OR
-    println(!(x==y || a==b && a > y))
-*/
-
-    val x = 7
-    val y = 6
-    if (x > y){ //curly brakets not necessary if only one line
-        println("hello, x is greater than y")
+    for(i in arr){
+        println(i)
     }
-    else if (x < y){
-        println("hello, x is less than y")
+
+    for(i in 1..3){
+        println(i)
     }
-    else{
-        println("hello, x is equal to y")
+
+    for(i in 7 downTo 3 step 3){
+        println(i)
+    }
+
+    for(i in 'a'..'z'){
+        println(i)
     }
 }

33a9153cadbdcfa882bdb264a118af839304dc90
RowBowl <robelrb@gmail.com>
2023-08-25 10:12:58 +0200

    more for loop practice. Get 5 numbers from the user and calculate the average

--- src/Tutorials.kt
+++ src/Tutorials.kt
@@ -1,12 +1,32 @@
 
 
 fun main () {
-    var arr = arrayOf(3,5,2,10,7,1)
-    var max = arr[0]
-    for(i in arr){
-        if(i > max){
-            max = i
+    //let user enter 5 numbers. Average the numbers and display it on the console
+
+    var userInputCount = 0
+    var arrInputs = emptyArray<Float>()
+    var sum = 0f
+    while(arrInputs.count() < 5){
+        val userInput = readlnOrNull()
+
+        if(userInput.isNullOrBlank() || userInput.isEmpty()){
+            println("Error: Please enter a number")
+        }
+        else if(!isNumeric(userInput)){
+            println("Error: Please enter numeric values only")
+        }
+        else{
+            arrInputs+= userInput.toFloat()
         }
     }
-    println(max)
+
+    for(i in arrInputs){
+        sum += i
+    }
+    println("The average value of these numbers is: ${sum/5}")
+}
+
+fun isNumeric(toCheck: String): Boolean {
+    val regex = "-?[0-9]+(\\.[0-9]+)?".toRegex()
+    return toCheck.matches(regex)
 }

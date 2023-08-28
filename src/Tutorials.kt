800d4c52d2400d0ca91ea0a8f531b564158a8f55
RowBowl <robelrb@gmail.com>
2023-08-28 22:40:49 +0200

    Introduction to when statements.

--- src/Tutorials.kt
+++ src/Tutorials.kt
@@ -1,32 +1,29 @@
 
 
 fun main () {
-    //let user enter 5 numbers. Average the numbers and display it on the console
+    val age = readLine()?.toInt()
 
-    var userInputCount = 0
-    var arrInputs = emptyArray<Float>()
-    var sum = 0f
-    while(arrInputs.count() < 5){
-        val userInput = readlnOrNull()
-
-        if(userInput.isNullOrBlank() || userInput.isEmpty()){
-            println("Error: Please enter a number")
-        }
-        else if(!isNumeric(userInput)){
-            println("Error: Please enter numeric values only")
-        }
-        else{
-            arrInputs+= userInput.toFloat()
-        }
+    //when your if/else statements depend on one variable, use when statements
+    when(age){
+        in 0..5 -> println("You're a young kid")
+        in 6..17 -> println("You're a teenager")
+        18 -> println("Finally, you're 18!")
+        19, 20 -> println("You're a young adult")
+        in 21..65 -> println("You're an adult")
+        else -> println("You're really old")
     }
-
-    for(i in arrInputs){
-        sum += i
-    }
-    println("The average value of these numbers is: ${sum/5.0}")
-}
-
-fun isNumeric(toCheck: String): Boolean {
-    val regex = "-?[0-9]+(\\.[0-9]+)?".toRegex()
-    return toCheck.matches(regex)
+/*
+    if(age in 0..5)
+        println("You're a young kid")
+    else if(age in 6..17)
+        println("You're a teenager")
+    else if(age == 18)
+        println("Finally, you're 18!")
+    else if(age == 19 || age == 20)
+        println("You're a young adult")
+    else if(age in 21..65)
+        println("You're an adult")
+    else
+        println("You're really old")
+        */
 }

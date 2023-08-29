c897a5d15e0f711378c60cd80cbc70c26d6468e9
RowBowl <robelrb@gmail.com>
2023-08-29 17:06:35 +0200

    more list practice

--- src/Tutorials.kt
+++ src/Tutorials.kt
@@ -1,28 +1,14 @@
 
 
 fun main () {
-    val array = arrayOf(1,2,3)
-    val list = listOf(1,2,3)
+    val list = mutableListOf<Int>()
+    for(i in 1..10){
+        val userInput = readLine()?.toInt()
 
-    //The values within a list are immutable, but the values of an array are mutable
-    array[0] = 3 //works fine
-    //list[0] = 3 does not work
+        if(userInput != null){
+            list.add(userInput)
+        }
+    }
 
-    //mutable list
-    val mList = mutableListOf(1,2,3)
-    mList[0] = 3
-
-    //values can be inserted into the list
-    mList.add(4)
-
-    //lists can be directly printed
-    println(mList)
-
-    //values are removable from lists
-    mList.remove(4)
-    mList.removeAt(0)
-    println(mList)
-
-
-    val mList1 =  mutableListOf<Int>()
+    println(list)
 }

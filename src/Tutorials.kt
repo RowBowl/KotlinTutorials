d6269e7ada59165d1d9e9e3289c400cd5e3ee9b2
RowBowl <robelrb@gmail.com>
2023-08-29 17:13:03 +0200

    lists hw (easy).

--- src/Tutorials.kt
+++ src/Tutorials.kt
@@ -1,14 +1,17 @@
 
 
 fun main () {
+    //accept 5 numbers from the user. Print them in reverse order
+
     val list = mutableListOf<Int>()
-    for(i in 1..10){
-        val userInput = readLine()?.toInt()
-
-        if(userInput != null){
-            list.add(userInput)
-        }
+    println("Please enter 5 numbers:")
+    for(i in 1..5){
+        val x = readLine()?.toInt()
+        if(x != null)
+            list.add(x)
     }
-
-    println(list)
+    println("The  5 numbers in reverse order are:")
+    for(i in list.reversed()){
+        println(i)
+    }
 }

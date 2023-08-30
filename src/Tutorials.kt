de6ccd5d5c962191820ad4e75dca15bc7c615213
RowBowl <robelrb@gmail.com>
2023-08-30 15:40:05 -0400

    More kotline vararg practice. Using default and named paramters.

--- src/Tutorials.kt
+++ src/Tutorials.kt
@@ -1,24 +1,14 @@
 
 
 fun main () {
-    val max = getMax(1,2,3) //no need to pass in list, simplypass in as many numbers you want
-    val max2 = getMax(4,3)
-    val arr =  intArrayOf(55,43,2,1,2) //intArrayof uses vararg
-
-    val max3 = getMax(1,3,44, *arr)
-
-    println("Max 1 is $max")
-    println("Max 2 is $max2")
-    println("Max 3 is $max3")
+    searchFor("How to use strings in kotlin")
+    searchFor("How to use strings in java", "Bing") //overwriting default value
+    searchFor(search="How to code", searchEngine = "Bing") //using named paramters, can be in any order
 
 }
 
-fun getMax(vararg numbers: Int): Int {
-    var max = numbers[0]
-    for(number in numbers){
-        if(number > max){
-            max = number
-        }
-    }
-    return max
+
+//hear, searchEngine paramter has a default value of "Google"
+fun searchFor(search: String, searchEngine: String = "Google") {
+    println("Searching for \"$search\" using $searchEngine")
 }

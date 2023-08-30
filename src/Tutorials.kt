2971450c0ed87c112547229e14bbc89dcc6b8d8f
RowBowl <robelrb@gmail.com>
2023-08-30 16:19:01 -0400

    More kotlin vararg practice.

--- src/Tutorials.kt
+++ src/Tutorials.kt
@@ -1,14 +1,31 @@
 
 
 fun main () {
-    searchFor("How to use strings in kotlin")
-    searchFor("How to use strings in java", "Bing") //overwriting default value
-    searchFor(search="How to code", searchEngine = "Bing") //using named paramters, can be in any order
+    val sum =  alternatingSum(3,4,5,2,1,2,3)
+    println("The alternating sum is $sum")
 
+    val sum2 =  alternatingSum(2,3,7,3)
+    println("The alternating sum is $sum2")
 }
 
+fun alternatingSum(vararg numbers: Int): Int {
+    var sum = 0
+    var switch = true
+    var firstIsDone = false
+    for(i in numbers) {
+        if(!firstIsDone){ //don't set switch if it's the first element
+            sum += i
+            firstIsDone = true
+        }
+        else if(switch) {
+            sum += i
+            switch = false
+        }
+        else {
+            sum -= i
+            switch = true
+        }
+    }
 
-//hear, searchEngine paramter has a default value of "Google"
-fun searchFor(search: String, searchEngine: String = "Google") {
-    println("Searching for \"$search\" using $searchEngine")
+    return sum
 }

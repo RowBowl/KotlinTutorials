f419795acc8579ca1a34530b5a5b876eb85b9768
RowBowl <robelrb@gmail.com>
2023-08-30 13:34:48 -0400

    fibbonaci sequence. Practice for using lists

--- src/Tutorials.kt
+++ src/Tutorials.kt
@@ -1,17 +1,20 @@
 
 
 fun main () {
-    //accept 5 numbers from the user. Print them in reverse order
+    //print n numbers of the fibbonaci sequence. n is inputted by the user
+    println("Enter a number > 1: ")
+    val userInput = readLine()?.toInt()
 
     val list = mutableListOf<Int>()
-    println("Please enter 5 numbers:")
-    for(i in 1..5){
-        val x = readLine()?.toInt()
-        if(x != null)
-            list.add(x)
+    if(userInput != null){
+        for(i in 0..<userInput){
+            if(i > 1){
+                list.add(list[i-2] + list[i-1])
+            } else{
+                list.add(i)
+            }
+        }
     }
-    println("The  5 numbers in reverse order are:")
-    for(i in list.reversed()){
-        println(i)
-    }
+    println(list)
+//[0,1,]
 }

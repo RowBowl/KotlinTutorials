9e66e2942b9a2fd0520cbd7e9c433ff3d07ae9f5
RowBowl <robelrb@gmail.com>
2023-08-30 14:19:27 -0400

    functions practice 3. functions with return value

--- src/Tutorials.kt
+++ src/Tutorials.kt
@@ -1,19 +1,14 @@
 
 
 fun main () {
-    var toNum = 5
-    sumTo(toNum)
-    toNum = 7
-    sumTo(toNum)
-    toNum = 15
-    sumTo(toNum)}
+    val pow = printPow(3, 5)
+    println("3 to the power of 5 is $pow")
+}
 
-fun sumTo(toNum: Int){
-    var result = 0
-    for(i in 1..toNum){
-        result += i
+fun printPow(base: Int, exponent: Int): Int {
+    var result = 1
+    for(i in 1..exponent){
+        result *= base
     }
-
-    println("The sum of the values from 1 to $toNum is $result")
-
+    return result
 }

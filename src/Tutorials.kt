0f51630ab9aff7a0e5a29e7d7ade5f3cf53495a9
RowBowl <robelrb@gmail.com>
2023-08-31 12:44:22 -0400

    Kotlin extension functions practice 2. Created extension function "product()" for Lists<Int>

--- src/Tutorials.kt
+++ src/Tutorials.kt
@@ -1,22 +1,14 @@
 
 
 fun main () {
-    println("Please enter a number: ")
-    val input = readLine()?.toInt()
-
-    if(input != null){
-        if(input.isPrime())
-            println("Input is a prime!")
-        else
-            println("Input is not a prime!")
-    }
+    var arr = listOf(1,2,3,4,5)
+    println("The product of ${arr} is ${arr.product()}")
 }
 
-fun Int.isPrime(): Boolean{
-    for(i in 2 until this-1){
-         if(this % i == 0)//divisible by a number other than 1 and itself
-             return false
+fun List<Int>.product(): Int{
+    var product = 1
+    for(i in this){
+        product *= i
     }
-    return true
-
+    return product
 }

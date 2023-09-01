6f821ff2156d7e8560c5f29fed85949c0f4caeee
RowBowl <robelrb@gmail.com>
2023-09-01 13:21:38 -0400

    More lambda function practice. Using list of objects and filtering them with lambda functions

--- src/Tutorials.kt
+++ src/Tutorials.kt
@@ -1,9 +1,17 @@
 
 
 fun main () {
-    var list = (1..20).toList()
-    println(list)
-    list = list.filter { it % 2 == 0 }
+    val circle1 = Circle(5.0)
+    val circle2 = Circle(3.5)
+    val triangle1 = Triangle(4.0, 4.0, 4.0)
+    val triangle2 = Triangle(3.0, 3.0, 3.0)
+    val rectangle1 = Rectangle(6.0)
+    val rectangle2 = Rectangle(4.0, 3.0)
 
-    println(list)
+    var shapes = listOf(circle1, circle2, triangle1, triangle2, rectangle1, rectangle2)
+    shapes = shapes.filter { it.area() > 20.0 }.sortedBy { it.area() }
+
+    for (shape in shapes) {
+        println("${shape.name}: Area: ${shape.area()}")
+    }
 }
